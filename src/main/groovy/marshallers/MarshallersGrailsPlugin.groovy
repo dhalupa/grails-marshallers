341f018876dfc834f339a84af4fbd0b70ae73794
Denis <denis.halupa@code-ex.com>
2016-04-19 22:17:18 +0200

    migration to grails 3

---
+++ src/main/groovy/marshallers/MarshallersGrailsPlugin.groovy
@@ -0,0 +1,78 @@
+package marshallers
+
+import grails.plugins.*
+import org.grails.plugins.marshallers.ExtendedConvertersConfigurationInitializer
+
+class MarshallersGrailsPlugin extends Plugin {
+
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "3.0.16 > *"
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def title = "Marshallers" // Headline display name of the plugin
+    def author = "Your name"
+    def authorEmail = ""
+    def description = '''\
+Brief summary/description of the plugin.
+'''
+    def profiles = ['web']
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/marshallers"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+//    def license = "APACHE"
+
+    // Details of company behind the plugin (if there is one)
+//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+
+    // Online location of the plugin's browseable source code.
+//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+
+    Closure doWithSpring() {
+        { ->
+            extendedConvertersConfigurationInitializer(ExtendedConvertersConfigurationInitializer)
+            ["xml", "json"].each { type ->
+                application."${type}MarshallerClasses".each { marshallerClass ->
+                    "${marshallerClass.fullName}"(marshallerClass.clazz) { bean ->
+                        bean.autowire = "byName"
+                    }
+                }
+            }
+        }
+    }
+
+    void doWithDynamicMethods() {
+        applicationContext.extendedConvertersConfigurationInitializer.initialize()
+        log.debug "Marshallers Plugin configured successfully"
+    }
+
+    void doWithApplicationContext() {
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    void onChange(Map<String, Object> event) {
+        event.ctx.extendedConvertersConfigurationInitializer.initialize()
+    }
+
+    void onConfigChange(Map<String, Object> event) {
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    void onShutdown(Map<String, Object> event) {
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
---
+++ src/main/groovy/org/grails/plugins/marshallers/test/MarshallerUnitSpecMixin.groovy
@@ -0,0 +1,28 @@
+package org.grails.plugins.marshallers.test
+
+
+
+import org.grails.plugins.marshallers.ExtendedConvertersConfigurationInitializer
+import org.grails.plugins.marshallers.JsonMarshallerArtefactHandler
+import org.grails.plugins.marshallers.XmlMarshallerArtefactHandler
+import org.grails.web.converters.configuration.ConvertersConfigurationInitializer
+
+class MarshallerUnitSpecMixin {
+
+	
+	def setup(){
+		// this has to be called first as there's no declarative way to enforce
+		// execution order
+		initGrailsApplication()
+
+		grailsApplication.registerArtefactHandler(new JsonMarshallerArtefactHandler())
+		grailsApplication.registerArtefactHandler(new XmlMarshallerArtefactHandler())
+		defineBeans {
+			convertersConfigurationInitializer(ConvertersConfigurationInitializer)
+			extendedConvertersConfigurationInitializer(ExtendedConvertersConfigurationInitializer)
+		}
+		applicationContext.convertersConfigurationInitializer.initialize(grailsApplication)
+		applicationContext.extendedConvertersConfigurationInitializer.initialize()
+	}
+
+}
